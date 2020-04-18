b43e8ef3308148cdf75b54dd63dea4be2cee4b9a
Leobhering <leobhering2009@hotmail.com>
2020-04-17 21:56:51 -0300

    APS, View e GateView testados e funcionando

---
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
@@ -0,0 +1,92 @@
+package br.pro.hashi.ensino.desagil.aps.view;
+import br.pro.hashi.ensino.desagil.aps.model.Gate;
+import br.pro.hashi.ensino.desagil.aps.model.Switch;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URL;
+
+public class GateView extends FixedPanel implements ItemListener {
+    private final Gate gate;
+    private final JCheckBox result;
+    private final JCheckBox[] inputs;
+    private final Switch[] switches;
+
+    private Color color;
+
+    public GateView(Gate gate) {
+        super(245, 346);
+
+        this.gate = gate;
+
+        int inputSize = gate.getInputSize();
+        switches = new Switch[inputSize];
+        this.inputs = new JCheckBox[inputSize];
+
+        for (int i=0; i < inputSize; i++) {
+            switches[i] = new Switch();
+            inputs[i] = new JCheckBox();
+            gate.connect(i, switches[i]);
+        }
+
+        result = new JCheckBox();
+
+        int margemEsq, margemCima;
+
+        margemEsq = 5;
+        margemCima = 25;
+
+        JLabel inputsLabel = new JLabel("Entrada");
+        add(inputsLabel, margemEsq, 10, 200, 15);
+
+        for (int i=0; i < inputSize; i++) {
+            add(inputs[i], margemEsq, margemCima + 20 * i, 200, 15);
+            inputs[i].addItemListener(this);
+        }
+
+        JLabel saidaLabel = new JLabel("Saida");
+        add(saidaLabel, margemEsq, margemCima + 20 * inputSize, 200, 15);
+        add(result, margemEsq, margemCima + 33 * inputSize, 200, 15);
+
+        color = Color.GREEN;
+
+        result.setEnabled(false);
+
+        update();
+    }
+
+    private void update() {
+        for (int i = 0; i < gate.getInputSize(); i++) {
+            if (inputs[i].isSelected()) {
+                switches[i].turnOn();
+            } else {
+                switches[i].turnOff();
+            }
+        }
+
+        boolean result = this.gate.read();
+        this.result.setSelected(result);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        // Não podemos esquecer desta linha, pois não somos os
+        // únicos responsáveis por desenhar o painel, como era
+        // o caso nos Desafios. Agora é preciso desenhar também
+        // componentes internas, e isso é feito pela superclasse.
+        super.paintComponent(g);
+
+        // Desenha um quadrado cheio.
+        g.setColor(color);
+
+        // Linha necessária para evitar atrasos
+        // de renderização em sistemas Linux.
+        getToolkit().sync();
+    }
+
+    public void itemStateChanged(ItemEvent itemEvent) {
+        update();
+    }
+}
